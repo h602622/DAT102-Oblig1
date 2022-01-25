e65879d06e925641ab0c1512eaf0354c1d9456dc
Anders Fimreite <602117@stud.hvl.no>
2022-01-25 12:30:28 +0100

    oppg3

--- src/no/hvl/dat102/Filmarkiv2.java
+++ src/no/hvl/dat102/Filmarkiv2.java
@@ -1,5 +1,7 @@
 package no.hvl.dat102;
 
+import java.util.ArrayList;
+
 import no.hvl.dat102.adt.FilmarkivADT;
 
 public class Filmarkiv2 implements FilmarkivADT {
@@ -9,49 +11,115 @@ public class Filmarkiv2 implements FilmarkivADT {
 
 	public Filmarkiv2() {
 		this.antall = 0;
-		this.start = new LinearNode<Film>();
+		this.start = null;
 	}
 
 	@Override
 	public void visFilm(int nr) {
-		// TODO Auto-generated method stub
-
+		LinearNode<Film> curr = this.start;
+		while (curr != null) {
+			if (nr == curr.getElement().getFilmNr()) {
+				System.out.println(curr.getElement().toString());
+				return;
+			}
+			curr = curr.getNext();
+		}
 	}
 
 	@Override
 	public void leggTilFilm(Film nyFilm) {
-		// TODO Auto-generated method stub
+		if (this.start == null) {
+			this.start = new LinearNode<Film>(nyFilm);
+			this.antall++;
+			return;
+		}
+		
+		LinearNode<Film> curr = this.start;
+		while (curr.getNext() != null) {
+			curr = curr.getNext();
+		}
 
+		curr.setNext(new LinearNode<Film>(nyFilm));
+		this.antall++;
 	}
 
 	@Override
 	public boolean slettFilm(int filmnr) {
-		// TODO Auto-generated method stub
-		return false;
+		if (this.start == null) {
+			return false;
+		}
+
+		LinearNode<Film> prev = null;
+		LinearNode<Film> curr = this.start;
+		while (curr.getElement().getFilmNr() != filmnr && curr.getNext() != null) {
+			prev = curr;
+			curr = curr.getNext();
+		}
+
+		if (curr.getElement().getFilmNr() != filmnr) {
+			return false;
+		}
+
+		if (prev == null) {
+			this.start = curr.getNext();
+			this.antall--;
+			return true;
+		}
+
+		prev.setNext(curr.getNext());
+		this.antall--;
+		return true;
 	}
 
 	@Override
 	public Film[] soekTittel(String delstreng) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Film> matcher = new ArrayList<Film>();
+
+		LinearNode<Film> curr = this.start;
+		while (curr != null) {
+			if (curr.getElement().getTittel().contains(delstreng)) {
+				matcher.add(curr.getElement());
+			}
+			curr = curr.getNext();
+		}
+
+		Film[] arr = new Film[matcher.size()];
+		arr = matcher.toArray(arr);
+		return arr;
 	}
 
 	@Override
 	public Film[] soekFilmprodusent(String delstreng) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Film> matcher = new ArrayList<Film>();
+
+		LinearNode<Film> curr = this.start;
+		while (curr != null) {
+			if (curr.getElement().getFilmSkaper().contains(delstreng)) {
+				matcher.add(curr.getElement());
+			}
+			curr = curr.getNext();
+		}
+
+		Film[] arr = new Film[matcher.size()];
+		arr = matcher.toArray(arr);
+		return arr;
 	}
 
 	@Override
 	public int antall(Sjanger sjanger) {
-		// TODO Auto-generated method stub
-		return 0;
+		int counter = 0;
+		LinearNode<Film> curr = this.start;
+		while (curr != null) {
+			if (sjanger == curr.getElement().getSjanger()) {
+				counter++;
+			}
+		}
+		return counter;
 	}
 
 	@Override
 	public int antall() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.antall;
 	}
 
 }
--- src/no/hvl/dat102/LinearNode.java
+++ src/no/hvl/dat102/LinearNode.java
@@ -31,4 +31,3 @@ public class LinearNode<T> {
 		return this.element;
 	}
 }
-
